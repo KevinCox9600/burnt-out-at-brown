210788a331170ecda202ea5777cbec619412b083
LeytonHo <leyton.ho@gmail.com>
2022-01-29 13:39:36 -0500

    Add script to scrape CAB

---
+++ scrape-cab.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+from seleniumwire import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from seleniumwire.utils import decode
+import json
+import jsonpickle
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+CAB_URL = "https://cab.brown.edu/"
+
+page = requests.get(CAB_URL)
+
+# Set up Beautiful Soup parser
+soup = BeautifulSoup(page.content, "html.parser")
+
+# Find all department codes
+results = soup.find(id="crit-dept")
+departments = [option['value'] for option in results.find_all('option')]
+filt_departments = str_list = list(filter(None, departments))
+
+# Might have to wait for this to load
+classes = {}
+for department_code in filt_departments:
+    driver.get('https://cab.brown.edu')
+
+
+    input_field = driver.find_element_by_id("crit-keyword")
+
+    input_field.send_keys(department_code)
+    input_field.send_keys(Keys.RETURN)
+    driver.find_element_by_id("search-button").click()
+
+    for request in driver.requests:
+        if request.response:
+
+            if request.url == f"https://cab.brown.edu/api/?page=fose&route=search&keyword={department_code}&is_ind_study=N&is_canc=N":
+                body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
+                str_body = body.decode('utf-8')
+                dict = json.loads(str_body)
+                results = dict["results"]
+
+                for r in results:
+                    code, title, time, prof = r["code"], r["title"], r["meets"], r["instr"]
+                    classes[code] = {title, time, prof}
+                    
+
+# Write classes to a JSON file
+classes_json = json.dumps(jsonpickle.encode(classes))
+class_list_file = open("class_list.json", "w")
+class_list_file.write(classes_json)
+class_list_file.close()
+
+driver.quit()
