21ada580fb722b8f18574ca1cf9e4ab26a82430a
LeytonHo <leyton.ho@gmail.com>
2022-01-29 18:58:02 -0500

    Loop through classes in scrape-cr

--- scrape-cr.py
+++ scrape-cr.py
@@ -1,12 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from cookie import COOKIE_CONSTANTS
+import json
+
+class_list_file = open('class_list.json')
+class_list_data = json.load(class_list_file)
+class_list_file.close()
 
 s = requests.Session()
-r = s.get('https://thecriticalreview.org/search/CSCI/0020', cookies=COOKIE_CONSTANTS)
 
-soup = BeautifulSoup(r.content, "html.parser")
-results = soup.find_all("div", {"class": "ui tiny statistic"})
+for c in class_list_data["data"]:
+    dept, num = c['dept'], c['num']
 
-for r in results[:6]:
-    print(" ".join(r.text.split()))
+    page = s.get(f"https://thecriticalreview.org/search/{dept}/{num}", cookies=COOKIE_CONSTANTS)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    for offering in soup.find_all("div", {"class": "ui tab"}):
+        # print(offering["data-edition"])
+        results = offering.find_all("div", {"class": "ui tiny statistic"})
+        for r in results[:6]:
+            # print(" ".join(r.text.split()))
+            pass
