2ce45e30c13e7271bc574a8d88880d4562dcc481
Jared Dunn <jdunn@webucator.com>
2022-01-29 18:46:37 -0500

    Add initial inputs

--- burnt-out-app/src/components/Filters.js
+++ burnt-out-app/src/components/Filters.js
@@ -1,5 +1,16 @@
 export default function Filters(type) {
   return (
-    <div>Filters</div>
+    <div>
+      <form>
+        <input type="text" className="form-control" placeholder="Department" />
+        <input type="text" className="form-control" placeholder="Professor" />
+        <input type="text" className="form-control" placeholder="Avg Hours" />
+        <input type="text" className="form-control" placeholder="Max Hours" />
+        <div className="form-check form-switch">
+          <input className="form-check-input" type="checkbox" role="switch" />
+          <label className="form-check-label" for="flexSwitchCheckDefault">Same professor</label>
+        </div>
+      </form>
+    </div>
   );
 }
