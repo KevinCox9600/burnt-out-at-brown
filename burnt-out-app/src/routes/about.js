b86370116eda44a22fe6ed51b73cd23b039f11af
Kevin Cox <kevindanielcox@gmail.com>
2022-01-29 12:31:25 -0500

    Reformat indentation

--- burnt-out-app/src/routes/about.js
+++ burnt-out-app/src/routes/about.js
@@ -1,7 +1,7 @@
 export default function About() {
-    return (
-        <main>
-            <h2>About</h2>
-        </main>
-    );
+  return (
+    <main>
+      <h2>About</h2>
+    </main>
+  );
 }
--- burnt-out-app/src/routes/profs.js
+++ burnt-out-app/src/routes/profs.js
@@ -1,7 +1,7 @@
 export default function Profs() {
-    return (
-        <main>
-            <h2>Profs</h2>
-        </main>
-    );
+  return (
+    <main>
+      <h2>Profs</h2>
+    </main>
+  );
 }
