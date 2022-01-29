58cb0dfc58498b1ad2205abe7f190d5d8be26753
Kevin Cox <kevindanielcox@gmail.com>
2022-01-29 13:37:07 -0500

    Create table using imported data

---
+++ burnt-out-app/src/components/Table.js
@@ -0,0 +1,56 @@
+function header(type) {
+  const courseHeader = (
+    <thead>
+      <tr>
+        <th scope="col">Avg Grade</th>
+        <th scope="col">Course</th>
+        <th scope="col">Avg Hours</th>
+        <th scope="col">Avg Max Hours</th>
+        <th scope="col">Time of day</th>
+        <th scope="col">Critical Review Link</th>
+        <th scope="col">Avg Class Size</th>
+      </tr>
+    </thead>
+  );
+  const profsHeader = (
+    <thead>
+      <tr>
+        <th scope="col">Avg Rating</th>
+        <th scope="col">Avg Grade</th>
+        <th scope="col">Avg Avg Hours</th>
+        <th scope="col">Avg Max Hours</th>
+        <th scope="col">Name</th>
+        <th scope="col">Department</th>
+        <th scope="col">Critical Review Link</th>
+      </tr>
+    </thead>
+  );
+
+  return type === "courses" ? courseHeader : profsHeader;
+}
+
+function rows(dataArray, type) {
+  return (
+    <tbody>
+      {dataArray.map((rowData, index) => (
+        type === "courses"
+          ? <CourseRow index={index} data={rowData} />
+          : <ProfRow index={index} data={rowData} />
+      ))}
+    </tbody>
+  );
+}
+
+export default function Table({ type }) {
+  const dataArray = [];
+
+  return (
+    <main>
+      <h2 style={{ textTransform: 'capitalize' }}>{type}</h2>
+      <table class="table">
+        {header(type)}
+        {rows(dataArray, type)}
+      </table>
+    </main>
+  );
+}
