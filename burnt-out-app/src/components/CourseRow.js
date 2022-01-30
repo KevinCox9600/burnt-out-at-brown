ddd47a7f7a9f5d92456e24a6a830747407b74b98
Kevin Cox <kevindanielcox@gmail.com>
2022-01-30 13:11:16 -0500

    Adapt for key name changes

--- burnt-out-app/src/components/CourseRow.js
+++ burnt-out-app/src/components/CourseRow.js
@@ -1,11 +1,11 @@
 export default function CourseRow({ data, sameProf }) {
-  const same = sameProf ? "same-prof" : "diff-prof";
+  const same = sameProf ? "same_prof" : "all_reviews";
   return (
     <tr>
-      <td>{data[same]["avg-grade"]}</td>
+      {/* <td>{data[same]["avg-grade"]}</td> */}
       <td>{data["code"] + " - " + data["name"]}</td>
-      <td>{data[same]["avg-hrs"]}</td>
-      <td>{data[same]["max-hrs"]}</td>
+      <td>{data[same]["avg_hrs"]}</td>
+      <td>{data[same]["max_hrs"]}</td>
       <td><a href={data["cr-link"]}>{data["cr-link"]}</a></td>
       <td>{data[same]["avg-size"]}</td>
       <td>{data["prof"]}</td>
--- burnt-out-app/src/components/Table.js
+++ burnt-out-app/src/components/Table.js
@@ -35,12 +35,12 @@ function header(type) {
 };
 
 function rows(dataArray, type, filterValues) {
-  const same = filterValues.sameProf ? "same-prof" : "diff-prof";
+  const same = filterValues.sameProf ? "same_prof" : "all_reviews";
   return (
     <tbody>
       {dataArray.filter((row) => {
-        const belowMaxHrs = parseInt(row[same]["max-hrs"]) <= filterValues.maxHrs;
-        const belowAvgHrs = parseInt(row[same]["avg-hrs"]) <= filterValues.avgHrs;
+        const belowMaxHrs = parseInt(row[same]["max_hrs"]) <= filterValues.maxHrs;
+        const belowAvgHrs = parseInt(row[same]["avg_hrs"]) <= filterValues.avgHrs;
         return belowMaxHrs && belowAvgHrs;
       }).map((rowData, index) => (
         type === "courses"
@@ -81,8 +81,9 @@ class Table extends React.Component {
   }
 
   render() {
-    const courses = require('../data/courses.json');
-    const dataArray = courses["data"];
+    // const courses = require('../data/courses.json');
+    // const dataArray = courses["data"];
+    const dataObj = require('../data/compiled_course_data.json');
 
     let filters;
     if (this.props.type === "courses") {
@@ -129,11 +130,17 @@ class Table extends React.Component {
 
     // filter the data
     let filteredDataArray = [];
-    let sameProfKey = filterValues.sameProf ? 'same-prof' : 'diff-prof';
-    for (let courseNum in dataArray) {
-      let courseData = dataArray[courseNum];
-      let underMaxHrs = courseData[sameProfKey]['max-hrs'];
-      let underAvgHrs = courseData[sameProfKey]['avg-hrs'];
+    let sameProfKey = filterValues.sameProf ? 'same_prof' : 'all_reviews';
+    // for (let courseNum in dataArray) {
+    for (let courseNum in dataObj) {
+      let courseData = dataObj[courseNum];
+      if (!courseData[sameProfKey]) {
+        continue;
+      }
+      console.log(courseNum);
+      console.log(courseData, sameProfKey, courseData[sameProfKey]);
+      let underMaxHrs = courseData[sameProfKey]['max_hrs'];
+      let underAvgHrs = courseData[sameProfKey]['avg_hrs'];
       let sameDept = courseData.dept.toLowerCase().includes(filterValues.dept.toLowerCase());;
       let correctProf = courseData.prof.toLowerCase().includes(filterValues.prof.toLowerCase());
       if (underMaxHrs && underAvgHrs && sameDept && correctProf) {
@@ -152,7 +159,7 @@ class Table extends React.Component {
         return 1;
       }
     });
-    filteredDataArray.sort((a, b) => a[sameProfKey]['avg-hrs'] - b[sameProfKey]['avg-hrs']);
+    filteredDataArray.sort((a, b) => a[sameProfKey]['avg_hrs'] - b[sameProfKey]['avg_hrs']);
 
     return (<main>
       <h2 style={{ textTransform: 'capitalize' }}>{this.props.type}</h2>;
