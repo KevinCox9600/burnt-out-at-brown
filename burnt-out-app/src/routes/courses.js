319254988e7818d06591378ae9aea948b87305cc
Jared Dunn <jdunn@webucator.com>
2022-01-29 21:12:06 -0500

    Move filters into table

--- burnt-out-app/src/routes/courses.js
+++ burnt-out-app/src/routes/courses.js
@@ -1,10 +1,8 @@
 import Table from '../components/Table';
-import Filters from '../components/Filters';
 
 export default function Courses() {
   return (
     <main>
-      <Filters type="courses" />
       <Table type="courses" />
     </main>
   );
--- burnt-out-app/src/routes/profs.js
+++ burnt-out-app/src/routes/profs.js
@@ -1,10 +1,8 @@
 import Table from '../components/Table';
-import Filters from '../components/Filters';
 
 export default function Profs() {
   return (
     <main>
-      <Filters type="profs" />
       <Table type="profs" />
     </main>
   );
