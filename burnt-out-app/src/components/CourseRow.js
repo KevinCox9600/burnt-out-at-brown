c2038e74952f7f94c1d8b7a4265483f318462814
Kevin Cox <kevindanielcox@gmail.com>
2022-01-30 22:45:59 -0500

    Make link open in new tab

--- burnt-out-app/src/components/CourseRow.js
+++ burnt-out-app/src/components/CourseRow.js
@@ -6,7 +6,7 @@ export default function CourseRow({ data, sameProf }) {
       <td>{Math.round(data[same]["avg_hrs"] * 100) / 100}</td>
       <td>{data["code"] + " - " + data["name"]}</td>
       <td>{Math.round(data[same]["max_hrs"] * 100) / 100}</td>
-      <td><a href={data["cr-link"]}>link</a></td>
+      <td><a href={data["cr-link"]} target="_blank" rel="noreferrer noopener">link</a></td>
       <td>{Math.round(data.size)}</td>
       <td>{data["prof"]}</td>
     </tr>
