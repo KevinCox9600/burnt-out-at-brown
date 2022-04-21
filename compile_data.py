c6ab661982599c5d76fb5f42711b95a2dceee201
Kevin Cox <kevindanielcox@gmail.com>
2022-04-21 15:57:25 -0400

    Refactor data compilation to use function

--- compile_data.py
+++ compile_data.py
@@ -18,52 +18,7 @@ def compile_data():
         # iterate over every cab course and find all CRs
         courses = {}
         for course in class_data["data"]:
-            # default fields
-            course_dict = {}
-            course_dict["dept"] = course["dept"]
-            course_dict["num"] = course["num"]
-            course_dict["code"] = course["dept"] + "" + course["num"]
-            course_dict["name"] = course["name"]
-            course_dict["prof"] = course["prof"]
-            course_dict["time"] = course["time"]
-
-            # calculated fields
-
-            # grab all reviews for course
-            course_crs = cr_data.get(course_dict["code"])
-            if not course_crs:
-                courses[course_dict["code"]] = course_dict
-                continue
-
-            # filter course reviews by same prof
-            course_crs_with_same_prof = list(
-                filter(lambda cr: cr["Prof"] == course["prof"], course_crs)
-            )
-
-            # general calculated fields
-            course_dict["size"] = reduce(
-                lambda acc, cr: acc + int(re.sub("[^0-9]", "", cr["Class Size"])),
-                course_crs,
-                0,
-            ) / len(course_crs)
-            course_dict["num-respondents"] = reduce(
-                lambda acc, cr: acc + int(cr["Respondents"]), course_crs, 0
-            ) / len(course_crs)
-            course_dict["link"] = (
-                "https://thecriticalreview.org/search/"
-                + f"{course['dept']}/{course['num']}"
-            )
-
-            course_dict["same_prof"] = {
-                "max_hrs": calc_max_hrs(course_crs_with_same_prof),
-                "avg_hrs": calc_avg_hrs(course_crs_with_same_prof),
-                "avg_rating": calc_avg_rating(course_crs_with_same_prof),
-            }
-            course_dict["all_reviews"] = {
-                "max_hrs": calc_max_hrs(course_crs),
-                "avg_hrs": calc_avg_hrs(course_crs),
-                "avg_rating": calc_avg_rating(course_crs),
-            }
+            course_dict = create_course_dict_from_course(course, cr_data)
 
             courses[course_dict["code"]] = course_dict
 
@@ -72,6 +27,56 @@ def compile_data():
     os.makedirs(os.path.dirname(COMPILED_DATA_FILE), exist_ok=True)
     with open(COMPILED_DATA_FILE, "w") as f:
         f.write(courses_json)
+
+
+def create_course_dict_from_course(course, cr_data):
+    """Creates a dictionary of compiled course data to be passed to the front end."""
+    # default fields
+    course_dict = {}
+    course_dict["dept"] = course["dept"]
+    course_dict["num"] = course["num"]
+    course_dict["code"] = course["dept"] + "" + course["num"]
+    course_dict["name"] = course["name"]
+    course_dict["prof"] = course["prof"]
+    course_dict["time"] = course["time"]
+
+    # calculated fields
+
+    # grab all reviews for course
+    course_crs = cr_data.get(course_dict["code"])
+    if not course_crs:
+        return course_dict
+
+    # filter course reviews by same prof
+    course_crs_with_same_prof = list(
+        filter(lambda cr: cr["Prof"] == course["prof"], course_crs)
+    )
+
+    # general calculated fields
+    course_dict["size"] = reduce(
+        lambda acc, cr: acc + int(re.sub("[^0-9]", "", cr["Class Size"])),
+        course_crs,
+        0,
+    ) / len(course_crs)
+    course_dict["num-respondents"] = reduce(
+        lambda acc, cr: acc + int(cr["Respondents"]), course_crs, 0
+    ) / len(course_crs)
+    course_dict["link"] = (
+        "https://thecriticalreview.org/search/" + f"{course['dept']}/{course['num']}"
+    )
+
+    course_dict["same_prof"] = {
+        "max_hrs": calc_max_hrs(course_crs_with_same_prof),
+        "avg_hrs": calc_avg_hrs(course_crs_with_same_prof),
+        "avg_rating": calc_avg_rating(course_crs_with_same_prof),
+    }
+    course_dict["all_reviews"] = {
+        "max_hrs": calc_max_hrs(course_crs),
+        "avg_hrs": calc_avg_hrs(course_crs),
+        "avg_rating": calc_avg_rating(course_crs),
+    }
+
+    return course_dict
 
 
 if __name__ == "__main__":
