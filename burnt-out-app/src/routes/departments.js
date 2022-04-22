e993c0879640f5f935c1b5cb549a4e46d215d3ff
Kevin Cox <kevindanielcox@gmail.com>
2022-04-21 20:52:16 -0400

    Add department table

--- burnt-out-app/src/routes/departments.js
+++ burnt-out-app/src/routes/departments.js
@@ -1,8 +1,43 @@
 import { DEFAULT_SEMESTER } from "../data/constants";
 
-export default function Departments() {
-  const departments = require(`../data/${DEFAULT_SEMESTER}/department_data.json`);
+function get_rows(departments) {
+  // build ordered list of department objects containing name and hours
+  departments.sort((a, b) => a.hours - b.hours);
+
+  // return all rows in a tbody tag
   return (
-    <h1>Departments by hours per week</h1>
+    <tbody>
+      {departments.map((dept, index) => {
+        return (
+          <tr>
+            <td>{dept.name}</td>
+            <td>{Math.round(dept.hours * 100) / 100}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+}
+
+export default function Departments() {
+  const departments_data = require(`../data/${DEFAULT_SEMESTER}/department_data.json`);
+  const departments = departments_data.data;
+  console.log(departments);
+
+  const rows = get_rows(departments);
+  return (
+    <div>
+      <h1>Departments by hours per week</h1>
+      <div className="table-responsive">
+        <table className="table" style={{ 'marginBottom': 0 }}>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          {rows}
+        </table>
+      </div>
+    </div>
   );
 }
