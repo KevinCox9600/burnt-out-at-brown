d33a911d15065646652230a1ab192b22a22d1f67
Kevin Cox <kevindanielcox@gmail.com>
2022-04-21 20:51:39 -0400

    Add department data file path

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # local constants
-SEASON = "spring"
-YEAR = "2023"
+SEASON = "fall"
+YEAR = "2022"
 semester = SEASON.lower() + YEAR
 
 # data constants
@@ -8,6 +8,7 @@ CLASS_LIST_FILE = f"./data/{semester}/class_list.json"
 CLASS_REVIEWS_LIST_FILE = f"./data/{semester}/class_objs.json"
 PROF_REVIEWS_LIST_FILE = f"./data/{semester}/prof_objs.json"
 COMPILED_DATA_FILE = f"./burnt-out-app/src/data/{semester}/compiled_course_data.json"
+DEPARTMENT_DATA_FILE = f"./burnt-out-app/src/data/{semester}/department_data.json"
 
 # urls
 CAB_URL = "https://cab.brown.edu/"
