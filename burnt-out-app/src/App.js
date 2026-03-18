06d85e0059bdbd1eb0431d94abed802ccb777563
Kevin Cox <kevindanielcox@gmail.com>
2026-03-18 15:35:35 -0700

    Make advert sticky

--- burnt-out-app/src/App.js
+++ burnt-out-app/src/App.js
@@ -8,27 +8,29 @@ import RecruitingAdvert from './components/RecruitingAdvert';
 function App({ to }) {
   return (
     <div>
-      <header className="sticky-top shadow-sm">
-        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "rgb(196, 164, 132)" }}>
-          <div className="container-fluid">
-            <Link to="/courses" className="navbar-brand"><img style={{ height: '64px' }} src='/burntout_brown.png' /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <NavLink to="/courses" className={({ isActive }) => isActive ? "active nav-link" : "nav-link"}>Courses</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/about" className={({ isActive }) => isActive ? "active nav-link" : "nav-link"}>About</NavLink>
-                </li>
-              </ul>
+      <div className="sticky-top shadow-sm">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "rgb(196, 164, 132)" }}>
+            <div className="container-fluid">
+              <Link to="/courses" className="navbar-brand"><img style={{ height: '64px' }} src='/burntout_brown.png' /></Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <NavLink to="/courses" className={({ isActive }) => isActive ? "active nav-link" : "nav-link"}>Courses</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/about" className={({ isActive }) => isActive ? "active nav-link" : "nav-link"}>About</NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </header>
-      <RecruitingAdvert />
+          </nav>
+        </header>
+        <RecruitingAdvert />
+      </div>
       <div id="page-content" className="container-fluid" style={{ padding: "0 20px 0 20px", marginBottom: '56px' }}>
         <Routes>
           <Route path="courses" element={<Courses />} />
