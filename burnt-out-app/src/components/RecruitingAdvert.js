e759af3d772372b64f9df79d303232b86665ec95
Kevin Cox <kevindanielcox@gmail.com>
2026-03-18 15:37:18 -0700

    Remove close button from advert

--- burnt-out-app/src/components/RecruitingAdvert.js
+++ burnt-out-app/src/components/RecruitingAdvert.js
@@ -6,17 +6,11 @@ class RecruitingAdvert extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dismissed: false,
     };
   }
 
-  dismiss() {
-    this.setState({ dismissed: true });
-  }
 
   render() {
-    if (this.state.dismissed) return null;
-
     return (
       <div id="recruiting-advert">
         <span>
@@ -25,13 +19,6 @@ class RecruitingAdvert extends React.Component {
             get involved (Apply by March 31)&rarr;
           </a> 
         </span>
-        <button
-          id="recruiting-advert-dismiss"
-          aria-label="Dismiss"
-          onClick={() => this.dismiss()}
-        >
-          &times;
-        </button>
       </div>
     );
   }
